b150f85d15aea0ac8f7a9df0c25de59e5e36ec61
balqees <balqeesalqudah97@gmail.com>
2023-05-31 16:45:01 +0300

    js code added

---
+++ app.js
@@ -0,0 +1,35 @@
+let Name= prompt("Please Enter Your Name");
+console.log(Name);
+
+let Gender=prompt("Enter your gender").toLowerCase();
+if(Gender=="female"){
+console.log(Gender)
+}else if (Gender=="male"){
+  console.log("male");
+}else { 
+ console.log(Gender);
+}
+
+let Age=prompt("Enter your Age");
+if (Age<=0) {
+  alert ("please re-enter your age"); 
+  prompt("Enter your Age");
+}else{
+  console.log(Age);
+}
+
+let conf=confirm( "Welcome to To Do Website" );
+
+
+  switch(Gender) { 
+
+  case "female": alert(("Welcome Ms "+ Name)); 
+  break; 
+
+  case "male" :alert ("Welcome Mr "+ Name);
+  break;
+
+  default: alert ("Welcome "+ Name);
+
+  }
+
